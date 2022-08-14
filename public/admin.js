cb2dd4e2cd6cc80344bb668f66c6e83e8bda8ec0
Ting-Yu Chen <7ingyu@gmail.com>
2022-08-14 11:25:49 -0700

    solution

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,89 @@
+const updateQuantity = async (el, id) => {
+  let sibling = el.previousSibling;
 
-// Your Code Here
+  console.log(sibling);
+  let requestBody = {
+    id, quantity: sibling.value
+  }
+
+  let res = await fetch('http://localhost:3001/updateBook', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id, quantity: sibling.value
+    })
+  })
+
+  console.log(res);
+}
+
+const main = async () => {
+  let root = document.querySelector('#root');
+
+  let res = await fetch('http://localhost:3001/listBooks');
+  let books = await res.json();
+  console.log(books);
+
+  // for (let i = 0; i < books.length; i++) {
+  //   let id = books[i].id;
+  //   let quantity = books[i].quantity;
+  //   let title = books[i].title;\
+
+  //   let wrapper = document.createElement('div');
+  //   wrapper.setAttribute('class', 'row');
+
+  //   let bookSpan = document.createElement('span');
+  //   bookSpan.innerHTML = title;
+  //   bookSpan.setAttribute('class', 'col')
+
+  //   let input = document.createElement('input');
+  //   input.setAttribute('type', 'number');
+  //   input.setAttribute('class', 'form-control col');
+  //   input.id = 'input' + id;
+  //   input.value = quantity;
+
+  //   let submit = document.createElement('input');
+  //   submit.setAttribute('type', 'submit');
+  //   submit.setAttribute('class', 'col btn btn-primary')
+  //   submit.value = 'Submit';
+
+  //   submit.setAttribute('onClick', `updateQuantity(this, ${id})`);
+  //   wrapper.append(bookSpan, input, submit);
+  //   root.append(wrapper);
+  // }
+
+  books.forEach((book) => {
+
+    let id = book.id;
+    let title = book.title;
+    let quantity = book.quantity;
+
+    let wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'row');
+
+    let bookSpan = document.createElement('span');
+    bookSpan.innerHTML = title;
+    bookSpan.setAttribute('class', 'col')
+
+    let input = document.createElement('input');
+    input.setAttribute('type', 'number');
+    input.setAttribute('class', 'form-control col');
+    input.id = 'input' + id;
+    input.value = quantity;
+
+    let submit = document.createElement('input');
+    submit.setAttribute('type', 'submit');
+    submit.setAttribute('class', 'col btn btn-primary')
+    submit.value = 'Submit';
+
+    submit.setAttribute('onClick', `updateQuantity(this, ${id})`);
+    wrapper.append(bookSpan, input, submit);
+    root.append(wrapper);
+  })
+
+}
+
+main();
+
